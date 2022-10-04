93f6a2ea15e86f41c859b0703b7e042aa2f6b651
Farwezahamed <112743043+Farwezahamed@users.noreply.github.com>
2022-10-04 22:53:37 +0530

    Add files via upload

---
+++ C Program/datatypes.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+printf("welcome to C programming.....");
+int age=19;
+char name[10]="Farwez";
+printf("\nMy name is...:%s",name);
+printf("\tMy age is...:%d",age);
+char c='a';
+char cc=100;
+const double pi=3.14;
+printf("\tMy pi value is.....%lf",pi);
+printf("\nThe size of pi variable is....%d",sizeof(pi));}
