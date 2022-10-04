20cecff6bfae8061720a630969f42052d7d78a4c
Farwezahamed <112743043+Farwezahamed@users.noreply.github.com>
2022-10-04 23:07:12 +0530

    Add files via upload

---
+++ JAVA Program/CastingExercise.java
@@ -0,0 +1,20 @@
+package JAVAPACKAGE;
+
+public class CastingExercise { 
+
+	public static void main(String[] args) { 
+	byte b=10; 
+	 
+	int i=b;
+	 
+	int c=i;
+	 
+	byte d=(byte)i;
+	 
+	byte x=10; 
+	byte y=20; 
+	 
+	int sum=x*y;
+	System.out.println(sum);
+	 } 
+	}
---
+++ JAVA Program/DoWhileLoop.java
@@ -0,0 +1,13 @@
+package JAVAPACKAGE;
+
+public class DoWhileLoop { 
+	 public static void main(String args[]) { 
+	 int x = 1; 
+	 
+	 do { 
+	 System.out.print("value of x : " + x ); 
+	 x++; 
+	 System.out.print("\n"); 
+	 }while( x < 11 ); 
+	 } 
+	}
